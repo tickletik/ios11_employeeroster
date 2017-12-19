48d7bd7428aef193c9b4ef01885b8917b7f585e0
Ronny Abraham <ronny.coder @gmail.com>
2017-12-19 11:03:42 +0200

    refactor name to EmployeeDetail

---
+++ EmployeeRoster/Controllers/EmployeeDetailTableVC.swift
@@ -0,0 +1,61 @@
+//
+//  EmployeeTableVC.swift
+//  EmployeeRoster
+//
+//  Created by ronny abraham on 12/19/17.
+//  Copyright © 2017 ronny abraham. All rights reserved.
+//
+
+import UIKit
+
+class EmployeeDetailTableVC: UITableViewController, UITextFieldDelegate {
+
+    struct PropertyKeys {
+        static let unwindToListIndentifier = "UnwindToListSegue"
+    }
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var dobLabel: UILabel!
+    @IBOutlet weak var employeeTypeLabel: UILabel!
+    
+    var employee: Employee?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        updateView()
+    }
+    
+    func updateView() {
+        if let employee = employee {
+            navigationItem.title = employee.name
+            nameTextField.text = employee.name
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateStyle = .medium
+            dobLabel.text = dateFormatter.string(from: employee.dateOfBirth)
+            dobLabel.textColor = .black
+            employeeTypeLabel.text = employee.employeeType.description()
+            employeeTypeLabel.textColor = .black
+        } else {
+            navigationItem.title = "New Employee"
+        }
+    }
+
+    @IBAction func saveButtonTapped(_ sender: UIButton) {
+        if let name = nameTextField.text {
+            employee = Employee(name: name, dateOfBirth: Date(), employeeType: .exempt)
+            performSegue(withIdentifier: PropertyKeys.unwindToListIndentifier, sender: self)
+        }
+    }
+
+    @IBAction func cancelButtonTapped(_ sender: UIButton) {
+        employee = nil
+        performSegue(withIdentifier: PropertyKeys.unwindToListIndentifier, sender: self)
+    }
+    
+    // MARK: - Text Field Delegate
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        return false
+    }
+}
